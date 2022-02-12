0b5da014e8b9ba694a198ef8d0540d8b59328353
Steve Hollasch <steve@hollasch.net>
2022-02-12 11:45:50 -0800

    WIP: camera changes in text

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -1,82 +1,80 @@
-//==============================================================================================
-// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related and
-// neighboring rights to this software to the public domain worldwide. This software is
-// distributed without any warranty.
-//
-// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
-// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//==============================================================================================
-
 #include "rtweekend.h"
 
-#include "camera.h"
 #include "color.h"
-#include "hittable_list.h"
-#include "material.h"
-#include "scene.h"
-#include "sphere.h"
+#include "ray.h"
+#include "vec3.h"
 
+#include <iostream>
 
-void random_spheres(scene& scene_desc) {
-    scene_desc.image_width       = 1200;
-    scene_desc.aspect_ratio      = 16.0 / 9.0;
-    scene_desc.samples_per_pixel = 10;
+class camera {
+  public:
+    camera() {
+        const auto aspect_ratio = 16.0 / 9.0;
+        const auto viewport_height = 2.0;
+        const auto viewport_width = aspect_ratio * viewport_height;
+        const auto focal_length = 1.0;
 
-    scene_desc.cam.vfov       = 20;
-    scene_desc.cam.focus_dist = 10.0;
-    scene_desc.cam.aperture   = 0.1;
-    scene_desc.cam.lookfrom   = vec3(13,2,3);
-    scene_desc.cam.lookat     = vec3(0,0,0);
-    scene_desc.cam.vup        = vec3(0,1,0);
+        focal_point = point3(0, 0, 0);
+        viewport_origin = point3(-viewport_width / 2, viewport_height / 2, -focal_length);
+        @@@
+    }
 
-    hittable_list& world = scene_desc.world;
+    // ??? Should the camera care about the image resolution?
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    ray get_ray(double s, double t) const {
+        auto u = s * step_x;
+        auto v = t * step_y;
+        auto pixel_loc = viewport_origin + u + v;
+        return ray(focal_point, pixel_loc - focal_point);
+    }
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+  private:
+    point3 focal_point;
+    point3 viewport_origin;
+}
 
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    double t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
 
-                if (choose_mat < 0.8) {
-                    // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else if (choose_mat < 0.95) {
-                    // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else {
-                    // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-            }
+int main() {
+
+    // Image
+
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+    auto focal_point = point3(0,0,0);
+
+    auto viewport_origin = point3(-viewport_width / 2, viewport_height / 2, -focal_length);
+    const int step_count_x = (image_width  < 2) ? 1 : image_width  - 1;
+    const int step_count_y = (image_height < 2) ? 1 : image_height - 1;
+    const auto step_x = vec3(static_cast<double>(viewport_width) / step_count_x, 0, 0);
+    const auto step_y = vec3(0, static_cast<double>(-viewport_height) / step_count_y, 0);
+
+    // Render
+
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        auto v = j * step_y;
+        for (int i = 0; i < image_width; ++i) {
+            auto u = i * step_x;
+            auto pixel_loc = viewport_origin + u + v;
+            ray r(focal_point, pixel_loc - focal_point);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color, 1);
         }
     }
 
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-}
-
-
-int main() {
-    scene scene_desc;
-    random_spheres(scene_desc);
-    scene_desc.render();
+    std::clog << "\rDone.                        \n";
 }
